72563fa3da5c21b1667fe2b96f55af0699b63ff2
Andreza Cardoso <77634842+cardosodeeh@users.noreply.github.com>
2022-03-07 09:56:55 -0300

    Update get.spec.js

--- treko-master/treko-master/api/test/get.spec.js
+++ treko-master/treko-master/api/test/get.spec.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import tasksModel from '../models/task'
 
 chai.use(chaiHttp);
 
@@ -8,7 +9,28 @@ const request = chai.request(app);
 const expect = chai.expect;
 
 describe('get', () => {
-    it('Deve retornar uma lista', (done) => {
-        done()
+
+    context('quando eu tenho tarefas cadastradas', () => {
+
+        before((done) => {
+            let tasks = [
+                {title: 'Estudar NodeJs', email: 'eu@andreza.io', done: false},
+                {title: 'Estudar MongoDB', email: 'eu@andreza.com', done: false},
+                {title: 'Estudar Lógica', email: 'eu@andreza.br', done: true}
+            ]
+
+            tasksModel.insertMany(tasks);
+            done();
+        })
+
+        it('Deve retornar uma lista', (done) => {
+            request
+                .get('/task')
+                .end((err,res) => {
+                    expect(res).to.has.status(200);
+                    expect(res.body.data).to.be.an('array');
+                    done();
+                })
+        })
     })
 })
